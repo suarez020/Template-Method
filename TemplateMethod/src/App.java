c648edf8e8d06b4062a0ac86ba310650a849f921
arley suarez agudelo <suarezhenry004@gmail.com>
2024-04-12 09:40:27 -0500

    .

--- TemplateMethod/src/App.java
+++ TemplateMethod/src/App.java
@@ -1,16 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Proceso proceso1 = new DocDataMiner();
-        Proceso proceso2 = new CSVDataminer();
-        Proceso proceso3 = new PDFDataMiner();
+        Proceso DOC = new DocDataMiner();
+        Proceso CSV = new CSVDataminer();
+        Proceso PDF = new PDFDataMiner();
 
         System.out.println("Ejecutando DocDataMiner:");
-        proceso1.ejecutarProceso();
+        DOC.ejecutarProceso();
 
         System.out.println("\nEjecutando CSVDataminer:");
-        proceso2.ejecutarProceso();
+        CSV.ejecutarProceso();
         
         System.out.println("\nEjecutando PDFDataMiner:");
-        proceso3.ejecutarProceso();
+        PDF.ejecutarProceso();
     }
 }
